998668c3d5e3f14e5b581bc345ddba1c455ede9b
Uchenna Nwaigwe <stupagha@gmail.com>
2024-09-17 20:15:07 +0100

    End of Adding Templating Engines

--- app.js
+++ app.js
@@ -4,18 +4,26 @@ const path = require("path");
 
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+//set the templating engine
+app.set("view engine", "ejs");
+
+//app.set("view engine", "pug");   //pug
+
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
   /* res.status(404).send("<h1>Page not found</h1>");*/
-  res.sendFile(path.join(__dirname, "views", "404.html"));
+  /*res.sendFile(path.join(__dirname, "views", "404.html"));*/
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: null });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,25 @@
-const path = require('path');
-const routDir = require('../util/path');
+const path = require("path");
+const routDir = require("../util/path");
 const express = require("express");
 const router = express.Router();
+const adminData = require("./admin");
 
-router.get('/', (req, res, next) => {
- /* res.send("<h1>Hello from Express!</h1>");*/
- /*res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));*/
- res.sendFile(path.join(routDir, 'views', 'shop.html'))
+router.get("/", (req, res, next) => {
+  /* res.send("<h1>Hello from Express!</h1>");*/
+  /*res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));*/
+
+  /*console.log('shop.js', adminData.products);
+ res.sendFile(path.join(routDir, 'views', 'shop.html'))*/
+
+  const products = adminData.products;
+  res.render("shop", {
+    prods: products,
+    pageTitle: "shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
