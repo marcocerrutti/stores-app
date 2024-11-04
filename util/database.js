17198ff7df0f985d438200ae4f8402d0627a3191
Uchenna Nwaigwe <hermessuffice@gmail.com>
2024-11-04 23:19:20 +0100

    End of Sequelize ORM-11

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'stores_complete',
-    password: 'Peacemaker7#'
+const sequelize = new Sequelize('stores_complete', 'root', 'Peacemaker7#', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
